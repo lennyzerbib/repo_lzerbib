fe55e7eedf05ae4b0873b16b60fdd1df24253692
lennyzerbib <zerbib.len@gmail.com>
2025-02-10 18:52:37 +0000

    main.py : modifying the path to the repo

--- streamlit_page/main.py
+++ streamlit_page/main.py
@@ -2,5 +2,5 @@ import sys
 import streamlit.web.cli as stcli
 
 if __name__ == '__main__':
-    sys.argv = ["streamlit","run","/Users/lennyzerbib/Desktop/Dauphine/streamlit_page/Home.py"]
+    sys.argv = ["streamlit","run","~/Desktop/repo_lzerbib/streamlit_page/Home.py"]
     sys.exit(stcli.main())
